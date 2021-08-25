71d2ff7309e31364eead76c3ffdc2574db33b1e0
Andre Esteves <estevesandre1@hotmail.com>
2021-08-25 16:02:51 +0100

    Use experiment to persist queries on refresh

--- app/src/lib/react-query.js
+++ app/src/lib/react-query.js
@@ -2,14 +2,17 @@
 // import { QueryClient, UseQueryOptions, UseMutationOptions, DefaultOptions } from 'react-query'
 // import { PromiseValue } from 'type-fest'
 
+import { persistQueryClient } from 'react-query/persistQueryClient-experimental'
+import { createWebStoragePersistor } from 'react-query/createWebStoragePersistor-experimental'
 import { QueryClient } from 'react-query'
 
 const queryConfig = {
   queries: {
     useErrorBoundary: true,
     refetchOnWindowFocus: false,
-    retry: false,
-    staleTime: 10000,
+    retry: 3,
+    staleTime: 86400000,
+    cacheTime: 86400000,
   },
 }
 
@@ -17,9 +20,17 @@ export const defaultQueryConfig = {
   queries: {
     useErrorBoundary: true,
     refetchOnWindowFocus: false,
-    retry: false,
-    staleTime: 10000,
+    retry: 3,
+    staleTime: 86400000,
+    cacheTime: 86400000,
   },
 }
 
 export const queryClient = new QueryClient({ defaultOptions: queryConfig })
+
+const localStoragePersistor = createWebStoragePersistor({ storage: window.localStorage })
+
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor
+})
