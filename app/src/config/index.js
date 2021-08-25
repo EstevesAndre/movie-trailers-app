66c2e0d2c85f309e29ff249f6794ad09555681d6
Andre Esteves <estevesandre1@hotmail.com>
2021-08-25 16:01:03 +0100

    Connect to back end

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.API_URL
+export const REACT_APP_API_URL = process.env.REACT_APP_API_URL
--- app/src/lib/axios.js
+++ app/src/lib/axios.js
@@ -1,10 +1,10 @@
 import Axios from 'axios'
 
-import { API_URL } from '@/config'
+import { REACT_APP_API_URL } from '@/config'
 // import { useNotificationStore } from '@/hooks/useNotificationStore'
 
 export const axios = Axios.create({
-  baseURL: API_URL,
+  baseURL: REACT_APP_API_URL,
 })
 
 axios.interceptors.response.use(
