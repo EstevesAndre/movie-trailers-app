95e9ea130bd0bcb1290201e2e4191278c530fbaa
Andre Esteves <estevesandre1@hotmail.com>
2021-08-28 03:28:51 +0100

    Update storage functions

--- app/src/utils/storage.js
+++ app/src/utils/storage.js
@@ -2,14 +2,22 @@ const storagePrefix = 'dept_the_case_'
 
 const storage = {
   getSearches: () => {
-    return JSON.parse(window.localStorage.getItem(`${storagePrefix}searches`))
+    return JSON.parse(window.localStorage.getItem(`${storagePrefix}searches`) || "[]")
   },
   setSearches: (searches) => {
     window.localStorage.setItem(`${storagePrefix}searches`, JSON.stringify(searches))
   },
+  addSearch: (search) => {
+    const searches = JSON.parse(window.localStorage.getItem(`${storagePrefix}searches`) || "[]")
+    window.localStorage.setItem(`${storagePrefix}searches`, JSON.stringify([search, ...searches.slice(0, 27).filter((item) => item.name !== search.name)]))
+  },
   clearSearches: () => {
     window.localStorage.removeItem(`${storagePrefix}searches`)
   },
+  removeSearch: (search) => {
+    const searches = JSON.parse(window.localStorage.getItem(`${storagePrefix}searches`) || "[]")
+    window.localStorage.setItem(`${storagePrefix}searches`, JSON.stringify(searches.slice(0, 27).filter((item) => item.name !== search.name)))
+  }
 }
 
 export default storage
