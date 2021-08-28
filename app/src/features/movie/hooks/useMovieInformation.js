9795225f4c3d8b351c34d59499af4b9aee25ca0c
Andre Esteves <estevesandre1@hotmail.com>
2021-08-28 19:58:26 +0100

    Update query config

--- app/src/features/movie/hooks/useMovieInformation.js
+++ app/src/features/movie/hooks/useMovieInformation.js
@@ -1,11 +1,11 @@
 import { useQuery } from 'react-query'
-import { defaultQueryConfig, freshQueryConfig } from '@/lib/react-query'
+import { defaultQueryConfig } from '@/lib/react-query'
 import { getMovieInformation, getMovieTrailers } from '../api'
 
 export const useMovieInformation = (imdb_id) => {
   return useQuery(
     {
-      ...freshQueryConfig,
+      ...defaultQueryConfig,
       enabled: Boolean(imdb_id),
       queryKey: ['movieInformation', imdb_id],
       queryFn: () => getMovieInformation(imdb_id),
