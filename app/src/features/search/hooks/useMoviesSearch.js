6dcb3922b10d30d20b86da228617cc0412cb50fd
Andre Esteves <estevesandre1@hotmail.com>
2021-08-28 03:28:36 +0100

    Add params to useQuery key

--- app/src/features/search/hooks/useMoviesSearch.js
+++ app/src/features/search/hooks/useMoviesSearch.js
@@ -2,13 +2,13 @@ import { useQuery } from 'react-query'
 import { freshQueryConfig } from '@/lib/react-query'
 import { getMoviesByTitle } from '../api'
 
-export const useMoviesByTitle = (search, offset = 0) => {
+export const useMoviesByTitle = (search, offset = 0, params = {}) => {
   return useQuery(
     {
       ...freshQueryConfig,
       enabled: Boolean(search),
-      queryKey: ['moviesByTitle', search, offset],
-      queryFn: () => getMoviesByTitle(search, offset),
+      queryKey: ['moviesByTitle', search, offset, JSON.stringify(params)],
+      queryFn: () => getMoviesByTitle(search, offset, params),
     }
   )
 }
