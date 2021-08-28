7d75452c0e42a8225465a857bff2f59a025ada42
Andre Esteves <estevesandre1@hotmail.com>
2021-08-28 19:55:27 +0100

    Update query config

--- app/src/features/search/hooks/useMoviesSearch.js
+++ app/src/features/search/hooks/useMoviesSearch.js
@@ -1,11 +1,11 @@
 import { useQuery } from 'react-query'
-import { freshQueryConfig } from '@/lib/react-query'
+import { defaultQueryConfig } from '@/lib/react-query'
 import { getMoviesByTitle } from '../api'
 
 export const useMoviesByTitle = (search, offset = 0, params = {}) => {
   return useQuery(
     {
-      ...freshQueryConfig,
+      ...defaultQueryConfig,
       enabled: Boolean(search),
       queryKey: ['moviesByTitle', search, offset, JSON.stringify(params)],
       queryFn: () => getMoviesByTitle(search, offset, params),
