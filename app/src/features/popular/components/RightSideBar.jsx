996a96c8bf4b5b8b8c75963cb8dc1d67f0db5c6b
Andre Esteves <estevesandre1@hotmail.com>
2021-08-28 19:57:17 +0100

    Remove scroll y

--- app/src/features/popular/components/RightSideBar.jsx
+++ app/src/features/popular/components/RightSideBar.jsx
@@ -4,10 +4,10 @@ export const RightSideBar = () => {
   return (
     <div className='flex-0 flex h-full'>
       <div className='px-10 lg:px-0 w-full lg:w-72 lg:bg-gray-900 flex flex-col lg:h-full lg:justify-start lg:items-center'>
-        <div className='h-1/2 w-full flex items-center justify-center'>
+        <div className='w-full flex items-center justify-center'>
           <PopularMovies className="pt-8 pb-2" />
         </div>
-        <div className='h-1/2 w-full flex items-center justify-center'>
+        <div className='w-full flex items-center justify-center'>
           <UpcomingMovies className="pb-5 pt-5" />
         </div>
       </div>
