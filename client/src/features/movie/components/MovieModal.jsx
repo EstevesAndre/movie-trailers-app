da25f635e65c51180f7ed45c0b537870a5421b7c
Andre Esteves <estevesandre1@hotmail.com>
2021-08-29 14:59:56 +0100

    Remove escape handler

--- client/src/features/movie/components/MovieModal.jsx
+++ client/src/features/movie/components/MovieModal.jsx
@@ -1,7 +1,7 @@
 // import { Button } from "@/components/Elements/Button/Button"
 import { ArrowCircleDownIcon, ChevronLeftIcon } from "@heroicons/react/outline"
 import clsx from "clsx"
-import { useState, useEffect, useCallback } from "react"
+import { useState } from "react"
 import { useMovieInformation } from "../hooks/useMovieInformation"
 import { Header } from "./Header"
 import { Trailers } from "./Trailers"
@@ -11,17 +11,17 @@ import { Similars } from "./Similars"
 
 const BaseModalComponent = ({ className, children, onClose = () => { }, isLoading, backdrop = null }) => {
 
-  const handleKeyPress = useCallback(event => {
-    if (event.key === 'Escape')
-      onClose()
-  }, [])
+  // const handleKeyPress = useCallback(event => {
+  //   if (event.key === 'Escape')
+  //     onClose()
+  // }, [])
 
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress)
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress)
-    }
-  }, [handleKeyPress])
+  // useEffect(() => {
+  //   window.addEventListener('keydown', handleKeyPress)
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeyPress)
+  //   }
+  // }, [handleKeyPress])
 
   return (
     <div className={clsx(
@@ -30,13 +30,13 @@ const BaseModalComponent = ({ className, children, onClose = () => { }, isLoadin
       className
     )}
     >
-      <div className="z-20 bg-gray-900 max-w-screen-2xl w-full sm:w-11/12 md:max-h-screen h-full sm:h-5/6 rounded-xl relative">
+      <div className="z-20 bg-gray-900 max-w-screen-2xl w-full sm:w-11/12 md:max-h-screen h-full sm:h-5/6 rounded-xl relative overflow-y-scroll">
         <div className={clsx("absolute z-30 w-full h-full flex items-start overflow-hidden rounded-xl", isLoading ? "skeleton-animation opacity-20" : "opacity-10")}>
           {backdrop &&
             <img src={backdrop} className="h-full opacity-70 adjust-img-ratio" />
           }
         </div>
-        <div className={clsx("flex flex-col w-full gap-y-4 absolute z-40 px-5 xs:px-10 lg:px-4 py-6 landscape-parent")} onKeyDown={handleKeyPress}>
+        <div className={clsx("flex flex-col w-full gap-y-4 absolute z-40 px-5 xs:px-10 lg:px-4 py-6 landscape-parent")}>
           <div className="fixed z-40">
             <ChevronLeftIcon className="h-8 w-8 cursor-pointer duration-100 ease-linear hover:text-red-600 text-gray-100" onClick={onClose} />
           </div>
