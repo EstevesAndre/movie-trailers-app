9508c2363478be8c25150892f2e4b5a932ac1256
Andre Esteves <estevesandre1@hotmail.com>
2021-08-29 21:13:51 +0100

    Remove dotenv config

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,5 @@
-const dotenv = require('dotenv')
-
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
-const envFound = dotenv.config()
-if (envFound.error) {
-  // This error should crash whole process
-
-  throw new Error("⚠️  Couldn't find .env file  ⚠️")
-}
 
 module.exports = {
   /**
